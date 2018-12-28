9506a149f1792a613a8f446aac6cd5bc89aff5b7
Tanaphon Auanhinkong <tanaphon.auanhinkong@ifyoucan.com>
2018-12-28 15:40:14 +0700

    use redux in BookListContainer

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
     div
   )
   ReactDOM.unmountComponentAtNode(div)
